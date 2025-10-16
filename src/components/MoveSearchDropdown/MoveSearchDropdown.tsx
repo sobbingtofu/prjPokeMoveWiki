b43006f88d573ff58e41882f320573477eef8f99
sobbingtofu <kukubasak@naver.com>
2025-10-16 13:57:31 +0900

    Feat : 드롭다운 컴포넌트화

---
+++ src/components/MoveSearchDropdown/MoveSearchDropdown.tsx
@@ -0,0 +1,48 @@
+import {koreanMoveType} from "@/logic/pokeapiLogics/type";
+
+function MoveSearchDropdown({
+  move: filteredMoves,
+  dropDownOnClick,
+}: {
+  move: koreanMoveType[];
+  dropDownOnClick: (move: koreanMoveType) => void;
+}) {
+  return (
+    <div
+      onMouseDown={(e) => e.preventDefault()}
+      className="sm:max-h-[160px] max-h-[120px] absolute top-full left-0 right-0 mt-1 bg-white border border-gray-300 rounded-lg shadow-lg  overflow-y-auto z-50"
+    >
+      {filteredMoves.map((move) => (
+        <div
+          key={move.id}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            dropDownOnClick(move);
+          }}
+          className="px-4 py-3 cursor-pointer hover:bg-gray-100 border-b border-gray-100 last:border-b-0 transition-colors duration-150"
+        >
+          <div className="flex gap-2.5 justify-between items-center">
+            <div className="flex-8 flex justify-start items-center ">
+              <p className=" text-gray-900 text-sm">{move.koreanName}</p>
+            </div>
+
+            <div className="w-1/12 flex justify-center items-center min-w-[40px]">
+              <p className="text-gray-500 text-sm font-bold ">
+                {move.damageClass === "physical" ? "물리" : move.damageClass === "special" ? "특수" : "변화"}
+              </p>
+            </div>
+            <div className="w-1/12 flex justify-center items-center min-w-[65px]">
+              <p
+                className={`rounded-full font-bold w-[65px] text-sm text-center text-white bg-${move.type.toLowerCase()} px-2.5 py-1.5`}
+              >
+                {move.korType}
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MoveSearchDropdown;
--- src/sections/SearchSection/SearchSection.tsx
+++ src/sections/SearchSection/SearchSection.tsx
@@ -7,6 +7,7 @@ import {CloseIcon} from "../../components/CloseIcon/CloseIcon";
 import {Loader} from "../../components/Loader/Loader";
 import {Toast} from "@/components/Toast/Toast";
 import {selectedMoveType} from "@/store/type";
+import MoveSearchDropdown from "@/components/MoveSearchDropdown/MoveSearchDropdown";
 
 interface SearchSectionProps {
   className?: string;
@@ -145,42 +146,7 @@ export const SearchSection = ({className = ""}: SearchSectionProps) => {
           </div>
 
           {/* 드롭다운 결과 */}
-          {isDropdownOpen && (
-            <div
-              onMouseDown={(e) => e.preventDefault()}
-              className="sm:max-h-[160px] max-h-[120px] absolute top-full left-0 right-0 mt-1 bg-white border border-gray-300 rounded-lg shadow-lg  overflow-y-auto z-50"
-            >
-              {filteredMoves.map((move) => (
-                <div
-                  key={move.id}
-                  onMouseDown={(e) => {
-                    e.preventDefault();
-                    handleDropdownItemClick(move);
-                  }}
-                  className="px-4 py-3 cursor-pointer hover:bg-gray-100 border-b border-gray-100 last:border-b-0 transition-colors duration-150"
-                >
-                  <div className="flex gap-2.5 justify-between items-center">
-                    <div className="flex-8 flex justify-start items-center ">
-                      <p className=" text-gray-900 text-sm">{move.koreanName}</p>
-                    </div>
-
-                    <div className="w-1/12 flex justify-center items-center min-w-[40px]">
-                      <p className="text-gray-500 text-sm font-bold ">
-                        {move.damageClass === "physical" ? "물리" : move.damageClass === "special" ? "특수" : "변화"}
-                      </p>
-                    </div>
-                    <div className="w-1/12 flex justify-center items-center min-w-[65px]">
-                      <p
-                        className={`rounded-full font-bold w-[65px] text-sm text-center text-white bg-${move.type.toLowerCase()} px-2.5 py-1.5`}
-                      >
-                        {move.korType}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
+          {isDropdownOpen && <MoveSearchDropdown move={filteredMoves} dropDownOnClick={handleDropdownItemClick} />}
 
           {/* 검색 결과 없을 시 메시지 */}
           {filteredMoves.length === 0 && searchValue.trim() !== "" && !isDebouncing && (
