7612283a0b2cf9de881d6615161d8b197821445c
STANDARDYUNLAPT\User <kijoonyun@smartfactory.co.kr>
2025-10-21 10:24:30 +0900

    Feat : 포켓몬 카드에 기술 별 배우는 정보 표시되도록 기본 로직 작성

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -2,7 +2,52 @@ import {detailedPokemInfoType} from "@/store/type";
 import React from "react";
 import TypeChip from "../TypeChip/TypeChip";
 
+interface LearnMethodDetail {
+  methodName: string;
+  learnedLevel: number;
+}
+
 function PokemonCard({pokemon}: {pokemon: detailedPokemInfoType}) {
+  const pokemonMoveDetails = pokemon.moveDetails || [];
+  const refinedMoveDetails = pokemonMoveDetails.map((move) => {
+    const versionMap = new Map<number, LearnMethodDetail[]>();
+    // genNumber를 키로, LearnMethodDetail 배열을 값으로 하는 Map
+
+    move.versionDetails.forEach((versionDetail) => {
+      if (versionDetail.genNumber) {
+        const genNumber = versionDetail.genNumber;
+        const learnMethodDetail: LearnMethodDetail = {
+          methodName: versionDetail.learnMethod,
+          learnedLevel: versionDetail.learnedLevel,
+        };
+
+        // 해당 genNumber가 이미 Map에 존재하는지 확인
+        if (versionMap.has(genNumber)) {
+          // 존재하면 learnMethod 배열에 추가
+          const methods = versionMap.get(genNumber)!;
+          // methodName이 중복되지 않으면 추가
+          if (!methods.some((m) => m.methodName === learnMethodDetail.methodName)) {
+            methods.push(learnMethodDetail);
+          }
+        } else {
+          // 존재하지 않으면 새로운 learnMethod 배열 생성
+          versionMap.set(genNumber, [learnMethodDetail]);
+        }
+      }
+    });
+
+    // Map을 배열로 변환
+    const uniqueVersionDetails = Array.from(versionMap, ([genNumber, learnMethods]) => ({
+      genNumber,
+      learnMethod: learnMethods,
+    }));
+
+    return {
+      moveKorName: move.moveKorName,
+      uniqueVersionDetails,
+    };
+  });
+
   return (
     <div
       key={pokemon.name}
@@ -19,16 +64,37 @@ function PokemonCard({pokemon}: {pokemon: detailedPokemInfoType}) {
           ))}
         </div>
       </div>
+
+      {/* 기술 별 배우는 세대 표시 */}
       <div>
         {pokemon.moveDetails &&
-          pokemon.moveDetails.length > 0 &&
-          pokemon.moveDetails.map((moveItem) => (
+          refinedMoveDetails.length > 0 &&
+          refinedMoveDetails.map((moveItem) => (
             <div key={moveItem.moveKorName} className="mb-2">
               <p className="font-bold">{moveItem.moveKorName}</p>
-              <div className="flex">
-                {moveItem.versionDetails.map((versionDetail, idx) => (
-                  <div key={idx} className="ml-2">
-                    <p>{versionDetail.versionName}</p>
+              <div className="flex flex-col">
+                {moveItem.uniqueVersionDetails.map((versionDetail, idx) => (
+                  <div key={idx} className="ml-2 flex gap-2">
+                    <p>{versionDetail.genNumber}</p>
+                    <div className="flex gap-1">
+                      {versionDetail.learnMethod.map((method, methodIdx) => {
+                        const methodNameMap: {[key: string]: string} = {
+                          "level-up": "레벨업",
+                          tutor: "기술가르침",
+                          machine: "기술머신",
+                          egg: "알",
+                        };
+
+                        const korMethodName = methodNameMap[method.methodName] || method.methodName;
+
+                        return (
+                          <div key={methodIdx} className="flex gap-1">
+                            <p>{korMethodName}</p>
+                            {method.learnedLevel > 0 && <p>Lv.{method.learnedLevel}</p>}
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
                 ))}
               </div>
