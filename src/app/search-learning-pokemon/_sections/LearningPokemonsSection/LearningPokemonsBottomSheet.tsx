9adc6e78e6ff9092ac67b4c0df823fc05a959e5e
sobbingtofu <kukubasak@naver.com>
2025-10-28 01:41:02 +0900

    Style : 모바일 화면 미세 스타일 조절

--- src/app/search-learning-pokemon/_sections/LearningPokemonsSection/LearningPokemonsBottomSheet.tsx
+++ src/app/search-learning-pokemon/_sections/LearningPokemonsSection/LearningPokemonsBottomSheet.tsx
@@ -18,13 +18,13 @@ function LearningPokemonsBottomSheet({className = ""}: LearningPokemonsBottomShe
     <section
       className={`sm:w-[60%] sm:min-w-[30px] w-full bg-gray-700 absolute
          rounded-t-xl z-500
-          h-full
+          h-dvh  overflow-hidden
           px-5 pt-0
           sm:hidden
           flex flex-col items-start gap-4
           transition-all duration-300 ease-in-out`}
       style={{
-        top: isLearningPokemonBottomSheetOpen ? "80px" : "calc(100vh - 100px)", // isShown 값에 따라 위치 조정
+        top: isLearningPokemonBottomSheetOpen ? "80px" : "calc(100vh - 120px)", // isShown 값에 따라 위치 조정
       }}
     >
       {/* 드래그 핸들 */}
