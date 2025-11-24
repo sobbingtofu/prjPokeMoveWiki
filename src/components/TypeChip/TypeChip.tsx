0a00beccbc438b0db4603e73422ea03bc7408780
STANDARDYUNLAPT\User <kijoonyun@smartfactory.co.kr>
2025-11-24 20:30:12 +0900

    Style : 스타일 미세 조정

--- src/components/TypeChip/TypeChip.tsx
+++ src/components/TypeChip/TypeChip.tsx
@@ -16,7 +16,7 @@ function TypeChip({
     <div
       className={` font-bold
         ${textSize == "2xs" ? "sm:rounded-lg rounded-md" : "sm:rounded-full rounded-lg"} 
-        ${textSize == "2xs" ? "w-[40px] sm:w-[45px]" : "w-[62px]"} 
+        ${textSize == "2xs" ? "w-10 sm:w-[45px]" : "w-[62px]"} 
         ${textSize == "2xs" ? "sm:text-[11px] text-[10px]" : `text-sm`} text-center text-white bg-${type.toLowerCase()}
         ${textSize == "2xs" ? "px-1 py-1" : "px-2.5 py-1.5"}
       `}
